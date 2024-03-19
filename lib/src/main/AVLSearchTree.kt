7f6baf265323bce1c59c4f288d935789350cb9d3
Nikita <nshchutskiy@gmail.com>
2024-03-19 21:14:29 +0300

    add AVLTree rotateLeft()

--- lib/src/main/AVLSearchTree.kt
+++ lib/src/main/AVLSearchTree.kt
@@ -4,6 +4,16 @@ class AVLSearchTree<K, V> : AbstractBinarySearchTree<K, V, AVLVertex<K,V>> {
 
     override fun remove(key: K): V? {TODO()}
 
+    private fun rotateLeft(curVertex: AVLVertex<K,V>) {
+        val rightSon : AVLVertex<K,V>  = (curVertex.rightSon as AVLVertex<K,V>)
+        curVertex.rightSon = rightSon.leftSon
+        rightSon.leftSon = curVertex
+        when (rightSon.sonsHeightDiff){
+            0 -> rightSon.sonsHeightDiff = 1
+            -1 -> {curVertex.sonsHeightDiff = 0; rightSon.sonsHeightDiff = 0}
+        }
+    }
+    
     constructor (comparator: Comparator<K>? = null) : super(comparator)
 
     constructor(map: Map<K, V>, comparator: Comparator<K>? = null) : super(map, comparator)
