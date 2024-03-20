035a038fbc3ecfa2f387ad4043fa3e4d03b603fa
Kostya <koreshin67@gmail.com>
2024-03-20 21:29:04 +0300

    feat: implement left and right rotates

--- lib/src/main/trees/RBSearchTree.kt
+++ lib/src/main/trees/RBSearchTree.kt
@@ -112,6 +112,32 @@ class RBSearchTree<K, V> : AbstractBinarySearchTree<K, V, RBVertex<K,V>> {
         return numOfChild
     }
 
+    //suppose that vertex has a rightSon
+    private fun rotateLeft(vertex: RBVertex<K, V>) {
+        val rightVertex: RBVertex<K, V>? = vertex.rightSon
+        vertex.rightSon = rightVertex?.leftSon
+        rightVertex?.leftSon.let { rightVertex?.leftSon?.parent = vertex }
+        rightVertex?.parent = vertex.parent
+        if (vertex.parent == null) root = rightVertex
+        else if (vertex == vertex.parent?.leftSon) vertex.parent?.leftSon = rightVertex
+        else vertex.parent?.rightSon = rightVertex
+        vertex.parent = rightVertex
+        rightVertex?.leftSon = vertex
+    }
+
+    //suppose that vertex has a leftSon
+    private fun rotateRight(vertex: RBVertex<K, V>) {
+        val leftVertex: RBVertex<K, V>? = vertex.leftSon
+        vertex.leftSon = leftVertex?.rightSon
+        leftVertex?.rightSon.let { leftVertex?.rightSon?.parent = vertex}
+        leftVertex?.parent = vertex.parent
+        if (vertex.parent == null) root = leftVertex
+        else if (vertex == vertex.parent?.leftSon) vertex.parent?.leftSon = leftVertex
+        else vertex.parent?.rightSon = leftVertex
+        vertex.parent = leftVertex
+        leftVertex?.rightSon = vertex
+    }
+
     constructor(comparator: Comparator<K>? = null) : super(comparator)
 
     constructor(map: Map<K, V>, comparator: Comparator<K>? = null) : super(map, comparator)
