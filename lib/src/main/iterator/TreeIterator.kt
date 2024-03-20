aca70076c4d0ff7c1544cd49a3e0628dd1e37f93
Victoria Lutsyuk <victorialutsyuk.t@gmail.com>
2024-03-20 12:02:49 +0300

    feat: add init block in TreeIterator class

--- lib/src/main/iterator/TreeIterator.kt
+++ lib/src/main/iterator/TreeIterator.kt
@@ -1,9 +1,16 @@
 package main.iterator
-import nodes.InterfaceBSTVertex
-import pair.Pair
+import main.nodes.InterfaceBSTVertex
+import main.pair.Pair
+import java.util.LinkedList
 
 internal class TreeIterator<K, V, N: InterfaceBSTVertex<K, V, N>>(
-    vertex: InterfaceBSTVertex<K, V, N>?): Iterator<Pair<K, V>> {
+    vertex: InterfaceBSTVertex<K, V, N>?
+): Iterator<Pair<K, V>> {
+    private val stack = LinkedList<N>()
+
+    init {
+        vertex?.let { stack.add(it as N) }
+    }
 
     override fun hasNext(): Boolean {TODO()}
 
