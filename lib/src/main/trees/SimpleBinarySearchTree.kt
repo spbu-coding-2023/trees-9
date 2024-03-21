32105d56f02e79a28e0f101184db03f5281cb5e0
Victoria Lutsyuk <victorialutsyuk.t@gmail.com>
2024-03-21 12:22:25 +0300

    feat: implement SimpleBinarySearchTree's private method putRec()

--- lib/src/main/trees/SimpleBinarySearchTree.kt
+++ lib/src/main/trees/SimpleBinarySearchTree.kt
@@ -1,9 +1,66 @@
 package main.trees
-import nodes.SimpleBSTVertex
+import main.nodes.SimpleBSTVertex
 
 class SimpleBinarySearchTree<K, V> : AbstractBinarySearchTree<K, V, SimpleBSTVertex<K,V>> {
 
-    override fun put(key: K, value: V) {TODO()}
+    override fun put(key: K, value: V, replaceIfExists: Boolean) {TODO()}
+
+    private fun putRec(key: K, value: V, replaceIfExists: Boolean, vertex: SimpleBSTVertex<K, V>? = root) {
+        if (root == null) {
+            root = SimpleBSTVertex(key, value)
+            return
+        }
+
+        val cpr = comparator
+
+        if (cpr != null && vertex != null) {
+            if (vertex.key == key && replaceIfExists) {
+                vertex.value = value
+                return
+            } else if (vertex.key == key && !replaceIfExists) {
+                return
+            }
+
+            if (cpr.compare(key, vertex.key) < 0) {
+                if (vertex.leftSon == null) {
+                    vertex.leftSon = SimpleBSTVertex(key, value)
+                    return
+                }
+                putRec(key, value, replaceIfExists, vertex.leftSon)
+            }
+            else {
+                if (vertex.rightSon == null) {
+                    vertex.rightSon = SimpleBSTVertex(key, value)
+                    return
+                }
+                putRec(key, value, replaceIfExists, vertex.rightSon)
+            }
+        }
+        else if (cpr == null && vertex != null) {
+            if (vertex.key == key && replaceIfExists) {
+                vertex.value = value
+                return
+            } else if (vertex.key == key && !replaceIfExists) {
+                return
+            }
+
+            val comparableKey: Comparable<K> = key as Comparable<K>
+            if (comparableKey.compareTo(vertex.key) < 0) {
+                if (vertex.leftSon == null) {
+                    vertex.leftSon = SimpleBSTVertex(key, value)
+                    return
+                }
+                putRec(key, value, replaceIfExists, vertex.leftSon)
+            }
+            else {
+                if (vertex.rightSon == null) {
+                    vertex.rightSon = SimpleBSTVertex(key, value)
+                    return
+                }
+                putRec(key, value, replaceIfExists, vertex.rightSon)
+            }
+        }
+    }
 
     override fun remove(key: K): V? {TODO()}
 
