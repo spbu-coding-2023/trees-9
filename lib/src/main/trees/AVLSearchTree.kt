308d49fa333a925b79eeb4ad418dd9f522421e3b
Nikita <nshchutskiy@gmail.com>
2024-03-22 08:08:13 +0300

    fix: change AVLTree rotate funcs constructors

--- lib/src/main/trees/AVLSearchTree.kt
+++ lib/src/main/trees/AVLSearchTree.kt
@@ -7,8 +7,7 @@ class AVLSearchTree<K, V> : AbstractBinarySearchTree<K, V, AVLVertex<K,V>> {
 
     override fun remove(key: K): V? {TODO()}
 
-    private fun rotateLeft(curVertex: AVLVertex<K,V>) : AVLVertex<K,V> {
-        val rightSon : AVLVertex<K,V>  = (curVertex.rightSon as AVLVertex<K,V>)
+    private fun rotateLeft(curVertex: AVLVertex<K,V>, rightSon : AVLVertex<K,V> ) : AVLVertex<K,V> {
         curVertex.rightSon = rightSon.leftSon
         rightSon.leftSon = curVertex
         when (rightSon.sonsHeightDiff) {
@@ -18,8 +17,7 @@ class AVLSearchTree<K, V> : AbstractBinarySearchTree<K, V, AVLVertex<K,V>> {
         return rightSon
     }
 
-    private fun rotateRight(curVertex: AVLVertex<K,V>) : AVLVertex<K,V> {
-        val leftSon : AVLVertex<K,V>  = (curVertex.leftSon as AVLVertex<K,V>)
+    private fun rotateRight(curVertex: AVLVertex<K,V>, leftSon : AVLVertex<K,V>) : AVLVertex<K,V> {
         curVertex.leftSon = leftSon.rightSon
         leftSon.rightSon = curVertex
         when (leftSon.sonsHeightDiff) {
@@ -29,14 +27,16 @@ class AVLSearchTree<K, V> : AbstractBinarySearchTree<K, V, AVLVertex<K,V>> {
         return leftSon
     }
     
-    private fun bigRotateLeft(curVertex: AVLVertex<K,V>) : AVLVertex<K,V> {
-        curVertex.rightSon = rotateRight(curVertex.rightSon as AVLVertex<K,V>)
-        return rotateLeft(curVertex)
+    private fun bigRotateLeft(curVertex: AVLVertex<K,V>, rightSon : AVLVertex<K,V> ) : AVLVertex<K,V> {
+        val curRightSon = rotateRight(rightSon, rightSon.leftSon as AVLVertex<K,V>)
+        curVertex.rightSon = curRightSon
+        return rotateLeft(curVertex, curRightSon)
     }
 
-    private fun bigRotateRight(curVertex: AVLVertex<K,V>) : AVLVertex<K,V> {
-        curVertex.leftSon = rotateLeft(curVertex.leftSon as AVLVertex<K,V>)
-        return rotateRight(curVertex)
+    private fun bigRotateRight(curVertex: AVLVertex<K,V>, leftSon : AVLVertex<K,V>) : AVLVertex<K,V> {
+        val curLeftSon = rotateLeft(leftSon, leftSon.rightSon as AVLVertex<K,V>)
+        curVertex.leftSon = curLeftSon
+        return rotateRight(curVertex, curLeftSon)
     }
 
     constructor (comparator: Comparator<K>? = null) : super(comparator)
