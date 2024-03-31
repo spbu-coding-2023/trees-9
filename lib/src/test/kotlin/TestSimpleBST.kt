d63c6d7f1cc87e29c556e84654533e5f86e83259
Victoria Lutsyuk <victorialutsyuk.t@gmail.com>
2024-03-31 19:20:41 +0300

    feat: add new class TestSimpleBST for tests which inherits SimpleBinarySearchTree class and write method getTreeRoot()

---
+++ lib/src/test/kotlin/TestSimpleBST.kt
@@ -0,0 +1,10 @@
+import main.trees.SimpleBinarySearchTree
+import main.vertexes.SimpleBSTVertex
+
+class TestSimpleBST<K, V> : SimpleBinarySearchTree<K, V> {
+    fun getTreeRoot(): SimpleBSTVertex<K, V>? {return root}
+
+    constructor(comparator: Comparator<K>?) : super(comparator)
+    constructor(map: Map<K, V>, replaceIfExists: Boolean = true, comparator: Comparator<K>? = null)
+            : super(map, replaceIfExists, comparator)
+}
