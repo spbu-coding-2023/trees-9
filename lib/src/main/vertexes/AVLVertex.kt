764490309f8b9fb32afa2c2c34e6b395de6735bf
Victoria Lutsyuk <victorialutsyuk.t@gmail.com>
2024-04-01 06:00:17 +0300

    dosc: add dosc to AVLVertex

--- lib/src/main/vertexes/AVLVertex.kt
+++ lib/src/main/vertexes/AVLVertex.kt
@@ -1,5 +1,16 @@
 package main.vertexes
 
+/**
+ * Represents a vertex in an AVL tree.
+ * @param K Type of keys.
+ * @param V Type of values.
+ * @property key The key associated with this vertex.
+ * @property value The value associated with this vertex.
+ * @property leftSon The left child vertex of this vertex, of type [AVLVertex].
+ * @property rightSon The right child vertex of this vertex, of type [AVLVertex].
+ * @property sonsHeightDiff The difference in height between the left and right subtrees.
+ */
+
 class AVLVertex<K, V>(
     override var key: K,
     override var value: V
@@ -7,8 +18,20 @@ class AVLVertex<K, V>(
 
     override var leftSon: AVLVertex<K, V>? = null
     override var rightSon: AVLVertex<K, V>? = null
+
+    /**
+     * The difference in height between the left and right subtrees.
+     */
     var sonsHeightDiff: Int = 0
 
+    /**
+     * Constructs a vertex with the specified key and value.
+     * @param key The key associated with this vertex.
+     * @param value The value associated with this vertex.
+     * @param leftSon The left child vertex of this vertex.
+     * @param rightSon The right child vertex of this vertex.
+     * @param sonsHeightDiff The difference in height between the left and right subtrees.
+     */
     constructor(
         key: K,
         value: V,
