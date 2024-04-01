d9ebfdbbf1bbdf844ef26a8f0e5a6a894d6873a9
Victoria Lutsyuk <victorialutsyuk.t@gmail.com>
2024-04-01 21:00:31 +0300

    fix: add default comparator value null in constructor

--- lib/src/test/kotlin/TestSimpleBST.kt
+++ lib/src/test/kotlin/TestSimpleBST.kt
@@ -4,7 +4,7 @@ import main.vertexes.SimpleBSTVertex
 class TestSimpleBST<K, V> : SimpleBinarySearchTree<K, V> {
     fun getTreeRoot(): SimpleBSTVertex<K, V>? {return root}
 
-    constructor(comparator: Comparator<K>?) : super(comparator)
+    constructor(comparator: Comparator<K>? = null) : super(comparator)
     constructor(map: Map<K, V>, replaceIfExists: Boolean = true, comparator: Comparator<K>? = null)
             : super(map, replaceIfExists, comparator)
 }
