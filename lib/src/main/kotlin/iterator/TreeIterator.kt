2a57ee2c4c3d6248574f3443aea7c98bdea6e1c1
Victoria Lutsyuk <victorialutsyuk.t@gmail.com>
2024-04-08 00:59:20 +0300

    docs(Iterator): refactor descriptions

--- lib/src/main/kotlin/iterator/TreeIterator.kt
+++ lib/src/main/kotlin/iterator/TreeIterator.kt
@@ -5,7 +5,9 @@ import java.util.Stack
 
 /**
  * Iterator iterates over the vertices of the tree, visiting each vertex in the order of a depth-first traversal.
- * Iterator interface implementation.
+ *
+ * [Iterator] interface implementation.
+ *
  * @param K the type of keys in the tree
  * @param V the type of values associated with the keys
  * @param N the type of tree vertices implementing InterfaceBSTVertex
@@ -22,7 +24,9 @@ open class TreeIterator<K, V, N : InterfaceBSTVertex<K, V, N>>(
 
     /**
      * Returns true if the iterator has more elements.
+     *
      * This method checks if there are more vertices to traverse in the tree.
+     *
      * @return true if the iterator has more elements, otherwise false
      */
     override fun hasNext(): Boolean {
@@ -31,7 +35,9 @@ open class TreeIterator<K, V, N : InterfaceBSTVertex<K, V, N>>(
 
     /**
      * Returns the next element in the iteration.
+     *
      * This method returns the next vertex in the depth-first traversal of the tree.
+     *
      * @return the next element in the iteration as a Pair containing the key and value of the vertex
      */
     override fun next(): Pair<K, V> {
